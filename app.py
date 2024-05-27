2bafea07561bb388e234288216a52646a4dfdf83
thoshistar <staraction0217@gmail.com>
2024-05-27 16:02:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ def generate():
 
 if __name__ == '__main__':
     os.makedirs("uploads", exist_ok=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
